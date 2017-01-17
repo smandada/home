c854345b087f6e5c90b171056da247df1c7d8155
Sandeep Mandada <s.mandada@gmail.com>
2017-01-16 20:52:38 -0800

    Add login api with jwt

--- userStory/app/routes/api.js
+++ userStory/app/routes/api.js
@@ -1,11 +1,22 @@
 var User = require('../models/user');
 var config = require('../../config');
 var secretKey = config.secretKey;
+var jwt = require('jsonwebtoken');
+
+function createToken(user) {
+  var token = jwt.sign({
+    _id: user.id,
+    name: user.name,
+    username: user.username
+  }, secretKey, {
+    expiresIn: '1d'
+  });
+  return token;
+}
 
 module.exports = function(app, express){
-
   var api = express.Router();
-
+  
   api.post('/signup', function(req, res){
     var user = new User({
       name: req.body.name,
@@ -17,7 +28,6 @@ module.exports = function(app, express){
         res.send(err);
         return;
       }
-
       res.json({ message: 'User has been created!'});
     });
   });
@@ -30,6 +40,29 @@ module.exports = function(app, express){
       }
       res.json(users);
     });
+  });
+
+  api.post('/login', function(req,res) {
+    User.findOne().select('password').exec(function(err, user){
+      if(err) throw err;
+
+      if(!user) {
+        res.send({ message: "User doesn't exist"});
+      } else if (user) {
+        var validPassword = user.comparePassword(req.body.password);
+
+        if (!validPassword) {
+          res.send( {message: "Invalid Password"} );
+        } else {
+          var token = createToken(user);
+          res.json({
+            success: true,
+            message: 'Successful',
+            token: token
+          });
+        }
+      }
+    })
   });
 
   return api;
